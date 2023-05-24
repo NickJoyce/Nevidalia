8611c6a243b54edd4707a14f6f9b78adc7c615a3
Nikita Smirnov <actan-spb@mail.ru>
2023-05-24 23:12:03 +0300

    initial commit

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
